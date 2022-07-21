9917ae8c05ca18f6b49205e370739e0a7eb28f9c
wachcio <wachcio@poczta.fm>
2022-07-22 01:04:59 +0200

    Fix import/export files types

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import * as path from 'path';
 import * as fs from 'fs';
-import { MulterDiskUploadedFiles } from 'types/files/files';
+import { MulterDiskUploadedFiles } from 'types';
 import { csvParse } from 'src/utils/csvParse';
 import { storageDir } from 'src/utils/storage';
 
