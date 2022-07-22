da880e7d2de315f311c6fdca2dc59fe9b1c447c9
Michal Molenda <michalus88@gmail.com>
2022-07-22 16:30:36 +0200

    implemented register with passport local strategy

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { HrRegisterDto } from '../hr/dto/hrRegister.dto';
 
-@Controller('user')
-export class UserController {}
+@Controller('api/user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('/hr')
+  hrRegister(@Body() hrRegisterDto: HrRegisterDto) {
+    return this.userService.hrRegister(hrRegisterDto);
+  }
+
+  @Post('/student')
+  studentRegister() {
+    return this.userService.studentRegister();
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { uuid } from 'uuidv4';
+import { User } from './user.entity';
+import { hashPwd } from '../utils/hash-pwd';
+import { Role, UserRes } from 'types';
+import { HrRegisterDto } from '../hr/dto/hrRegister.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  async getByEmail(email: string): Promise<User | null> {
+    return await User.findOne({
+      where: {
+        email,
+      },
+    });
+  }
+
+  async hrRegister(hrRegisterDto: HrRegisterDto) {
+    const { email, firstName, lastName, company, maxReservedStudents } =
+      hrRegisterDto;
+    await this.checkingEmailAvailability(email);
+
+    const user = new User();
+    const salt = uuid();
+    const password = uuid();
+    const registerToken = uuid();
+    user.email = email;
+    user.password = user.password = hashPwd(password, salt);
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.role = Role.HR;
+    user.salt = salt;
+    user.registerToken = registerToken;
+
+    await user.save();
+    return { user, password };
+    // return sanitizeUser(user);
+  }
+
+  async studentRegister(): Promise<UserRes> {
+    //TUTAJ MUSISZ ZROBIĆ VALIDACJĘ POD KONTEM @ W EMAILU
+
+    // await this.checkingEmailAvailability(email) // walidacja czy już istnieje w bazie
+
+    const user = new User();
+    const salt = uuid();
+    const password = uuid();
+    const registerToken = uuid();
+    // user.email = email; //dodasz jak wyciągniesz emaila z pliku
+    user.password = user.password = hashPwd(password, salt);
+    //POLE fullName będzie dodane przy aktywacji studenta
+    user.role = Role.STUDENT;
+    user.salt = salt;
+    user.registerToken = registerToken;
+
+    await user.save();
+    return user;
+    // return sanitizeUser(user);
+  }
+
+  async checkingEmailAvailability(email) {
+    if (await this.getByEmail(email)) {
+      throw new BadRequestException(
+        'The user with the given email already exists.',
+      );
+    }
+  }
+}
