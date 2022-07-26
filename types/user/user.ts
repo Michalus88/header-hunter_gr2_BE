c183ac57491d900b750327566c4a089fad7eb36e
Michal Molenda <michalus88@gmail.com>
2022-07-26 23:10:17 +0200

    modified UserData and UserRes interfaces

--- types/user/user.ts
+++ types/user/user.ts
@@ -8,10 +8,13 @@ export interface UserData {
   id: string;
   isActive: boolean;
   role: Role;
-  firstName: string;
-  lastName: string;
   email: string;
   password: string;
+  salt: string;
+  registerToken: string | null;
 }
 
-export type UserRes = Omit<UserData, 'password' | 'isActive'>;
+export type UserRes = Omit<
+  UserData,
+  'password' | 'isActive' | 'salt' | 'registerToken'
+>;
