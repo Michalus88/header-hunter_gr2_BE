7981c32d38bd15ece86dcab72397a3133a51a449
Michal Molenda <michalus88@gmail.com>
2022-07-27 21:38:56 +0200

    removed class validator dekorators

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,6 +1,5 @@
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { Role, UserData } from 'types';
-import { IsNotEmpty } from 'class-validator';
 
 @Entity()
 export class User extends BaseEntity implements UserData {
@@ -11,12 +10,6 @@ export class User extends BaseEntity implements UserData {
     default: false,
   })
   isActive: boolean;
-
-  @Column({ nullable: true, length: 255 })
-  firstName: string;
-
-  @Column({ nullable: true, length: 255 })
-  lastName: string;
 
   @Column({
     length: 255,
@@ -29,14 +22,12 @@ export class User extends BaseEntity implements UserData {
   })
   password: string;
 
-  @IsNotEmpty()
   @Column({
     type: 'enum',
     enum: Role,
   })
   role: Role;
 
-  @IsNotEmpty()
   @Column({
     length: 255,
   })
