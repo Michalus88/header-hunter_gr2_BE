68dcad2a2e5c183ccd87ea43b09670b9f1d6ee1f
Michal Molenda <michalus88@gmail.com>
2022-07-28 10:04:35 +0200

    deleted file

--- src/mailerconfig.ts
+++
@@ -1,9 +0,0 @@
-export const mailConfig = {
-  transport: {
-    host: 'smtp.sendgrid.net',
-    auth: {
-      user: process.env.SENDGRID_USER_NAME,
-      pass: process.env.SENDGRID_PASS,
-    },
-  },
-};
