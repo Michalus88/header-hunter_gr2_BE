0f8816adefcf70987ebc318c33f32bf39ac13624
Michal Molenda <michalus88@gmail.com>
2022-08-02 11:40:04 +0200

    implemented get reserved-students

--- src/hr/hr.controller.ts
+++ src/hr/hr.controller.ts
@@ -3,16 +3,25 @@ import { HrService } from './hr.service';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import { UserObj } from '../decorators/user-object.decorator';
 import { User } from '../user/user.entity';
+import { StudentService } from '../student/student.service';
 
 @Controller('api/hr')
 export class HrController {
-  constructor(private readonly hrService: HrService) {}
+  constructor(
+    private readonly hrService: HrService,
+    private readonly studentService: StudentService,
+  ) {}
 
   // @UseGuards(JwtAuthGuard)
   // @Get('/profile')
   // getOne(@UserObj() user: User) {
   //   return this.hrService.getStudentsForInterview(user);
   // }
+  @UseGuards(JwtAuthGuard)
+  @Get('/reserved-students')
+  getReservedStudents(@UserObj() user: User) {
+    return this.studentService.getReservedStudents(user);
+  }
 
   @UseGuards(JwtAuthGuard)
   @Patch('/booking-student/:studentId')
