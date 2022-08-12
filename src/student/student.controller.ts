814c1fbd9093032efeff2b07595ed3c1c0643963
Michal Molenda <michalus88@gmail.com>
2022-08-12 14:08:13 +0200

    removed parameters and used try catch

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -28,17 +28,10 @@ export class StudentController {
   @Put('/:studentId')
   @UseGuards(JwtAuthGuard, IsStudent)
   @UseInterceptors(GithubUserValidate, EmailExistInDBValidate)
-  studentProfileUpdate(
-    @UserObj() user: User,
-    @Body() studentProfileUpdateDto,
-    @Param('studentId') studentId: string,
-    @Res() res: Response,
-  ) {
+  studentProfileUpdate(@UserObj() user: User, @Body() studentProfileUpdateDto) {
     return this.studentService.studentProfileUpdate(
       user,
       studentProfileUpdateDto,
-      studentId,
-      res,
     );
   }
 
