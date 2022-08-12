20e2cefed63b047fd3a4aa3e6e18ba59ec7b4c3b
wachcio <wachcio@poczta.fm>
2022-08-12 16:51:58 +0200

    Add Swagger UI

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { GlobalExceptionFilter } from './filter/global-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Header hunter')
+    .setDescription('Przykłady zapytań do API dla aplikacji Header hunter')
+    .setVersion('1.0')
+    .addTag('header hunter')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
   app.use(cookieParser());
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalPipes(
