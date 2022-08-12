7c7fe94052b266371c09ce512b228d6d0dbd49ed
Michal Molenda <michalus88@gmail.com>
2022-08-12 23:56:43 +0200

    blocked login for hired student

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,12 +11,16 @@ import { UserService } from 'src/user/user.service';
 import { hashPwd } from 'src/utils/hash-pwd';
 import { sanitizeUser } from '../utils/sanitize-user';
 import { stringToBoolean } from '../utils/string-to-boolean';
+import { DataSource } from 'typeorm';
+import { StudentProfile } from '../student/student-profile.entity';
+import { Role, StudentStatus } from '../../types';
 
 @Injectable()
 export class AuthService {
   constructor(
     @Inject(forwardRef(() => UserService))
     private userService: UserService,
+    private dataSource: DataSource,
   ) {}
 
   async validateUser(email: string, password: string): Promise<any> {
@@ -27,7 +31,7 @@ export class AuthService {
     return null;
   }
 
-  login(user: User, res: Response) {
+  async login(user: User, res: Response) {
     const payload = { email: user.email };
     const token = sign(payload, process.env.JWT_SECRET, { expiresIn: '1d' });
     const oneDay = 1000 * 60 * 60 * 24;
@@ -35,6 +39,17 @@ export class AuthService {
       throw new BadRequestException(
         'Your account is inactive. Please check Your mail and click to activation link.',
       );
+    }
+    if (user.role === Role.STUDENT) {
+      const student = await this.dataSource
+        .createQueryBuilder()
+        .select('student.status')
+        .from(StudentProfile, 'student')
+        .where('student.userId = :userId', { userId: user.id })
+        .getOne();
+      if (student.status === StudentStatus.HIRED) {
+        throw new BadRequestException('You are hired and you lost access.');
+      }
     }
     return res
       .cookie('jwt', token, {
