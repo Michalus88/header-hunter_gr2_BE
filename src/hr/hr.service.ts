d0ae9fa6a4e5638dbc09344d55c8e5b9aec985c2
Michal Molenda <michalus88@gmail.com>
2022-08-14 00:22:00 +0200

    added getFilteredBookingStudents and extractedHrFieldsFromUser

--- src/hr/hr.service.ts
+++ src/hr/hr.service.ts
@@ -1,10 +1,16 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { User } from '../user/user.entity';
 import { HrProfile } from './hr-profile.entity';
-import { LoggedUserRes } from '../../types';
+import { FilteringOptions, LoggedUserRes, StudentStatus } from '../../types';
 import { DataSource } from 'typeorm';
 import { StudentService } from '../student/student.service';
 import { ReservationService } from '../reservation/reservation.service';
+import { FilteringOptionsDto } from '../student/dto/filtering-options.dto';
+import { filteringQueryBuilder } from '../utils/filtering-query-builder';
 
 @Injectable()
 export class HrService {
@@ -15,13 +21,10 @@ export class HrService {
   ) {}
 
   async getMe(user: User): Promise<LoggedUserRes> {
-    const hr = await this.dataSource
-      .createQueryBuilder()
-      .select(['hrProfile.firstName', 'hrProfile.lastName'])
-      .from(HrProfile, 'hrProfile')
-      .where('hrProfile.user = :userId', { userId: user.id })
-      .getOne();
-
+    const hr = await this.extractedHrFieldsFromUser(user, [
+      'hr.firstName',
+      'hr.lastName',
+    ]);
     return { id: user.id, role: user.role, ...hr, email: user.email };
   }
 
@@ -29,19 +32,9 @@ export class HrService {
     const studentProfile = await this.studentService.getAvailableStudent(
       studentId,
     );
-    const hrProfile = await this.dataSource
-      .createQueryBuilder()
-      .select('hrProfile')
-      .from(HrProfile, 'hrProfile')
-      .where('hrProfile.user = :userId', { userId: user.id })
-      .getOne();
-    if (!hrProfile) {
-      throw new ForbiddenException(
-        'You are not logged in or Your data has been modified.',
-      );
-    }
-    await this.reservationService.isStudentBooked(hrProfile, studentId);
-    await this.reservationService.add(hrProfile, studentProfile);
+    const hr = await this.extractedHrFieldsFromUser(user);
+    await this.reservationService.isStudentBooked(hr, studentId);
+    await this.reservationService.add(hr, studentProfile);
 
     return {
       codeStatus: 200,
@@ -49,15 +42,61 @@ export class HrService {
     };
   }
 
-  async getBookedStudents(user: User) {
+  async getBookedStudents(user: User, filteringOptions?: FilteringOptions) {
     await this.reservationService.verificationStudentBookingTime();
-    const hr = await this.dataSource
-      .createQueryBuilder()
-      .select('hr.id')
-      .from(HrProfile, 'hr')
-      .where('hr.user = :userId', { userId: user.id })
-      .getOne();
+    const hr = await this.extractedHrFieldsFromUser(user, ['hr.id']);
+    const { id } = hr;
+    return this.reservationService.getBookedStudents(id);
+  }
 
-    return this.reservationService.getBookedStudents(hr);
+  async getFilteredBookingStudents(
+    user: User,
+    filteringOptions: FilteringOptionsDto,
+  ) {
+    const hr = await this.extractedHrFieldsFromUser(user, ['hr.id']);
+    const { id } = hr;
+    const {
+      expectedContractType,
+      teamProjectDegree,
+      canTakeApprenticeship,
+      courseEngagement,
+      courseCompletion,
+      monthsOfCommercialExp,
+      projectDegree,
+    } = filteringOptions;
+    const parameters = {
+      expectedContractType,
+      teamProjectDegree,
+      canTakeApprenticeship,
+      courseEngagement,
+      courseCompletion,
+      monthsOfCommercialExp,
+      projectDegree,
+    };
+    const query = filteringQueryBuilder(filteringOptions);
+
+    return this.reservationService.getBookedStudents(id, query, parameters);
+  }
+
+  async extractedHrFieldsFromUser(user: User, extractedFields: string[] = []) {
+    const selections: string[] = extractedFields;
+    if (extractedFields.length === 0) {
+      selections.push('hr');
+    }
+    try {
+      const hr = await this.dataSource
+        .createQueryBuilder()
+        .select(selections)
+        .from(HrProfile, 'hr')
+        .where('hr.user = :userId', { userId: user.id })
+        .getOne();
+      if (!hr) {
+        throw new BadRequestException('User with hr role does not exist.');
+      }
+
+      return hr;
+    } catch (error) {
+      throw new BadRequestException('Wrong field name.');
+    }
   }
 }
